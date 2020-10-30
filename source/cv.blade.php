43faddbbe3ee0e4b492a4552893235a513143feb
Alex Powell <apowell@clickenergy.com.au>
2020-10-30 14:28:49 +1100

    more tweaks to layout

--- source/cv.blade.php
+++ source/cv.blade.php
@@ -15,36 +15,36 @@
                     <svg xmlns='http://www.w3.org/2000/svg' class="inline-block h-5 mr-1 fill-current -mt-05 sm:-ml-6" viewBox='0 0 512 512'><title>Checkmark Circle</title><path d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z' fill='none' stroke='currentColor' stroke-miterlimit='10' stroke-width='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M352 176L217.6 336 160 272'/></svg>
                     Key Accomplishments</h4>
                 <ol class="mb-8">
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://www.onthemove.com.au/unilodge" target="_blank">
                                 On The Move Embedded Utilities Application Form
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2020</small>
                         </h3>
                         <p class="leading-snug">A new way for Real Estate Agencies to embedded a utility connection form within their website, where a new tenant or purchaser can connect all their utilities completely within the agencies site; with no offsite redirection.</p>
                     </li>
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://rebels.clickenergy.com.au" target="_blank">
                                 Melbourne Rebels Partner Site
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2019</small>
                         </h3>
                         <p class="leading-snug">A showcase of Click Energy's commitment to partner with the Melbourne Rebels and Rugby Union. A co-branded website specifically for the Rebels. We are your energy!</p>
                     </li>
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://rewards.onthemove.com.au" target="_blank">
                                 On The Move Partner Portal
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2018</small>
@@ -52,12 +52,12 @@
                         <p class="leading-snug">A portal for <a href="https://onthemove.com.au" target="_blank">On The Move</a>'s Real Estate Agent and Property Manager partners to submit leads and integration with our rewards redemption platform. It also functions as a clearing house for external integrations with real estate software and CRMs.</p>
                     </li>
                     
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://www.amaysim.com.au/energy" target="_blank">
                                 amaysim Energy launch
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2017</small>
@@ -65,12 +65,12 @@
                         <p class="leading-snug">Technical delivery leader to launch the customer facing amaysim energy brand. From sales funnels, quoting tool, in addition to BAU functions - like paying a bill, or adding a concession to your account. White labelling and integrating Click's existing assets with amaysim's brand a systems - resulting in tens of thousands of sales from a ~5 month project.</p>
                     </li>
                     
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://www.clickenergy.com.au" target="_blank">
                                 Click Energy website redesign
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2015</small>
@@ -80,12 +80,12 @@
 
                     
 
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 <a class="hover:text-primary-500" href="https://www.clickenergy.com.au/quick-quote-calculator" target="_blank">
                                     Click Energy Quick Quote Calculator
-                                    <ion-icon class="text-sm" name="open"></ion-icon>
+                                    <svg xmlns='http://www.w3.org/2000/svg' class='inline-block h-4 fill-current -mt-05' viewBox='0 0 512 512'><title>Open</title><path d='M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
                                 </a>
                             </span>
                             <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2014</small>
@@ -96,42 +96,42 @@
                     
                 </ol>
 
-                <h4 class="mb-4 tracking-wide text-gray-700 uppercase font-700">
+                <h4 class="mt-8 mb-4 tracking-wide text-gray-700 uppercase font-700">
     
                     <svg xmlns='http://www.w3.org/2000/svg' class="inline-block h-5 fill-current mr-05 -mt-05 sm:-ml-6" viewBox='0 0 512 512'><title>Ribbon</title><path d='M269 335.61q-6.33.47-12.78.47c-5.23 0-10.4-.24-15.51-.69a176.11 176.11 0 01-127.67-72.94 4 4 0 00-6.77.35l-72 129.4c-2.74 5-3.17 11-.28 15.88A16.78 16.78 0 0048.22 416h78a15.28 15.28 0 0113.62 7.33L178.5 488a16.26 16.26 0 0013.75 8c5.94-.33 12.09-4.19 14.56-9.6l66.11-145.15a4 4 0 00-3.92-5.64zM477.64 391.88l-71.53-129.17a4 4 0 00-6.74-.36 176.5 176.5 0 01-78.31 61.42 16.09 16.09 0 00-8.72 8.25l-36.86 81.1a7.92 7.92 0 000 6.6l30.27 66.59c2.45 5.41 8.59 9.36 14.52 9.69a16.3 16.3 0 0013.7-8.12l38.53-64.58c2.89-4.85 8.13-7.33 13.78-7.3h78.77c6.67 0 11.72-3.48 14-10a16.92 16.92 0 00-1.41-14.12z'/><ellipse cx='256.26' cy='160' rx='48.01' ry='48'/><path d='M256.26 16c-79.42 0-144 64.59-144 144s64.61 144 144 144 144-64.6 144-144-64.59-144-144-144zm0 224a80 80 0 1180-80 80.1 80.1 0 01-80 80z'/></svg>
                     
                     Awards</h4>
                 <dl class="mb-8">
                     
-                    <dt class="flex items-baseline justify-between text-lg">
+                    <dt class="flex items-baseline justify-between mb-2 text-lg">
                         <span>Quarterly Values Award &ndash; Reliability</span>
                         <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">Q2&mdash;2020</small>
                     </dt>
-                    <dd class="mb-2 italic text-gray-700 lg:w-2/3">Quarterly winners are nominated by peers, who represent reliability. We build reliable products full of fail safes and back-ups. Reliability is also about being committed to the amaysim way and being responsible and authentic in our behavior.</dd>
+                    <dd class="mb-4 italic text-gray-700 lg:w-3/4">Quarterly winners are nominated by peers, who represent reliability. We build reliable products full of fail safes and back-ups. Reliability is also about being committed to the amaysim way and being responsible and authentic in our behavior.</dd>
                     
-                    <dt class="flex items-baseline justify-between text-lg">
+                    <dt class="flex items-baseline justify-between mb-2 text-lg">
                         <span>Quarterly Values Award &ndash; Agility</span>
                         <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">Q1&mdash;2020</small>
                     </dt>
-                    <dd class="mb-2 italic text-gray-700 lg:w-2/3">Quarterly winners are nominated by peers, who represent agility. Agility is about reacting quickly, applying mental flexibility when a problem needs to be solved and always holding on to the entrepreneurial spirit.</dd>
+                    <dd class="mb-4 italic text-gray-700 lg:w-3/4">Quarterly winners are nominated by peers, who represent agility. Agility is about reacting quickly, applying mental flexibility when a problem needs to be solved and always holding on to the entrepreneurial spirit.</dd>
 
-                    <dt class="flex items-baseline justify-between text-lg">
+                    <dt class="flex items-baseline justify-between mb-2 text-lg">
                         <span>Outside the Box Thinker Award</span>
                         <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2018</small>
                     </dt>
-                    <dd class="mb-2 italic text-gray-700 lg:w-2/3">Awarded to an individual who finds creative and divergent ways of solving problems across the business.</dd>
+                    <dd class="mb-4 italic text-gray-700 lg:w-3/4">Awarded to an individual who finds creative and divergent ways of solving problems across the business.</dd>
                     
-                    <dt class="flex items-baseline justify-between text-lg">
+                    <dt class="flex items-baseline justify-between mb-2 text-lg">
                         <span>Above and Beyond Award</span>
                         <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2017</small>
                     </dt>
-                    <dd class="mb-2 italic text-gray-700 lg:w-2/3">Awarded to someone who goes beyond of what's required of them and delivers exceptional value to the business.</dd>
+                    <dd class="mb-4 italic text-gray-700 lg:w-3/4">Awarded to someone who goes beyond of what's required of them and delivers exceptional value to the business.</dd>
                     
-                    <dt class="flex items-baseline justify-between text-lg">
+                    <dt class="flex items-baseline justify-between mb-2 text-lg">
                         <span>Manager Directors' Award</span>
                         <small class="text-sm text-right text-gray-600 uppercase tracking-copperplate">2016</small>
                     </dt>
-                    <dd class="mb-2 italic text-gray-700 lg:w-2/3">The "MVP" of the year, awarded at the discretion of the Managing Director <small>(or CEO in other years)</small> to the individual who has made the most impact to the business for the year.</dd>
+                    <dd class="mb-4 italic text-gray-700 lg:w-3/4">The "MVP" of the year, awarded at the discretion of the Managing Director <small>(or CEO in other years)</small> to the individual who has made the most impact to the business for the year.</dd>
                     
                 </dl>
             </li>
@@ -145,7 +145,7 @@
                     <svg xmlns='http://www.w3.org/2000/svg' class="inline-block h-5 mr-1 fill-current -mt-05 sm:-ml-6" viewBox='0 0 512 512'><title>Checkmark Circle</title><path d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z' fill='none' stroke='currentColor' stroke-miterlimit='10' stroke-width='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M352 176L217.6 336 160 272'/></svg>
                     Key Accomplishments</h4>
                 <ol class="mb-8">
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 Natvia
@@ -154,7 +154,7 @@
                         </h3>
                         <p class="leading-snug">The brand website for Natvia. WordPress backed for easy updating by other teams, and translated into 6 different languages.</p>
                     </li>
-                    <li class="mb-4">
+                    <li class="mb-6">
                         <h3 class="flex items-baseline justify-between mb-2">
                             <span class="text-lg text-gray-800 font-500">
                                 
@@ -174,7 +174,7 @@
         <h2 class="mb-6 font-serif text-3xl tracking-tight font-500">Education</h2>
 
         <ol>
-            <li class="mb-4">
+            <li class="mb-6">
                 <p class="flex items-baseline justify-between">
                     <span class="text-lg text-gray-800 font-500">
                         Bachelor of Design <small class="uppercase">(Multimedia Design)</small>
@@ -183,7 +183,7 @@
                 </p>
                 <h5 class="text-sm text-gray-600 uppercase tracking-copperplate">Swinburne University of Technology</h5>
             </li>
-            <li class="mb-4">
+            <li class="mb-6">
                 <p class="flex items-baseline justify-between">
                     <span class="text-lg text-gray-800 font-500">
                         Advanced Diploma of Multimedia
@@ -192,7 +192,7 @@
                 </p>
                 <h5 class="text-sm text-gray-600 uppercase tracking-copperplate">Swinburne TAFE</h5>
             </li>
-            <li class="mb-4">
+            <li class="mb-6">
                 <p class="flex items-baseline justify-between">
                     <span class="text-lg text-gray-800 font-500">
                         Certificate <abbr title="Four" class="cursor-pointer">IV</abbr> of Multimedia
